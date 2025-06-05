bc0871c47361543057850e22640849ba952a859a
ankit kumar <ankitchahar88@gmail.com>
2025-06-05 18:41:31 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
+let count = 0;
 
-let count = 0
-
-function increase() {
-    count = count + 1
-    updateCounter()
-    console.log("Counter increased")
-}
-
-function decrease() {
-    count = count - 1
-    updateCounter()
-    console.log("Counter decreased")
-}
-
-function reset() {
-    count == 0
-    updateCounter()
-    console.log("Counter reset")
-}
 
 function updateCounter() {
-    document.getElementById('count').text = count
+    document.getElementById('count').innerText = count;
 }
 
-document.getElementById('greetBtn').addEventListener("click", function() {
-    const name = document.getElementById("name").value
-    if(name = "") {
-        alert("Please enter your name")
-    } else {
-        document.getElementById("welcome").innerText = "Welcome, " + name
-    }
-})
+
+function increase() {
+    count += 1;
+    updateCounter();
+}
+
+
+function decrease() {
+    count -= 1;
+    updateCounter();
+}
+
+
+function reset() {
+    count = 0;
+    updateCounter();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("greetBtn").addEventListener("click", function () {
+        const name = document.getElementById("name").value.trim();
+        if (name === "") {
+            alert("Please enter your name");
+        } else {
+            document.getElementById("welcome").innerText = "Welcome, " + name;
+        }
+    });
+
+    updateCounter(); 
+});
