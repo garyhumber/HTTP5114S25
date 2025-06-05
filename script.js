5d8f7a2c8f60f06a3727c95c559b6eab946d3822
Phurten Jang Sherpa <n01716188@humber.ca>
2025-06-05 18:10:15 -0400

    fixed inut field, buttons and fonts

--- script.js
+++ script.js
@@ -2,32 +2,33 @@
 let count = 0
 
 function increase() {
-    count = count + 1
-    updateCounter()
-    console.log("Counter increased")
+    count = count + 1;
+    updateCounter();
+    console.log("Counter increased");
 }
 
+
 function decrease() {
-    count = count - 1
-    updateCounter()
-    console.log("Counter decreased")
+    count = count - 1;
+    updateCounter();
+    console.log("Counter decreased");
 }
 
 function reset() {
-    count == 0
-    updateCounter()
-    console.log("Counter reset")
+    count == 0;
+    updateCounter();
+    console.log("Counter reset");
 }
 
 function updateCounter() {
-    document.getElementById('count').text = count
+    document.getElementById('count').text = count;
 }
 
 document.getElementById('greetBtn').addEventListener("click", function() {
-    const name = document.getElementById("name").value
+    let name = document.getElementById("name").value;
     if(name = "") {
-        alert("Please enter your name")
+        alert("Please enter your name");
     } else {
-        document.getElementById("welcome").innerText = "Welcome, " + name
+        document.getElementById("welcome").innerText = "Welcome, " + name;
     }
 })
